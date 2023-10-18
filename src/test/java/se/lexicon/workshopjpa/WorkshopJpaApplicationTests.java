32e147abb5625fee305503ff0b5c36087a358b2c
Liebniz <anders.leif.loren@liebniz.com>
2023-10-18 09:50:08 +0200

    Initial commit

---
+++ src/test/java/se/lexicon/workshopjpa/WorkshopJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.workshopjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
